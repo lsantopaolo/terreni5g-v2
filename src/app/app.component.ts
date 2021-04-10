21514afba96bb09640804bc743f86e52010e2bfe
lsantopaolo <santopaolo.luigi@gmail.com>
2021-04-10 14:45:08 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  name = 'Terreni 5G ' + VERSION.major;
 
   constructor( private router: Router) { 
     
